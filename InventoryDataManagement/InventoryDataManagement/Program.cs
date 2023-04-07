ed7ef613f33b3af51167ca18c168135e0e6b0c16
unknown <bhotnikhil25@gmail.com>
2023-04-07 23:19:51 +0530

    [Nikhil]-Inventory Data Management

---
+++ InventoryDataManagement/InventoryDataManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryDataManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inventory Data Management");
+        }
+    }
+}
